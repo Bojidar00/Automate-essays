1d19ae497070a7b551088205506f5c399b9ffab6
Bojidar00 <bojidar_rusalinov@abv.bg>
2021-11-24 13:53:59 +0200

    Add file name check.

---
+++ main.js
@@ -0,0 +1,27 @@
+var dir = require('node-dir');
+const fs = require('fs');
+
+var directory="essays";
+
+dir.subdirs(directory, function(err, subdirs) {
+    if (err) throw err;
+    
+    for (let i = 0; i < subdirs.length; i++) {
+        console.log(subdirs[i]);
+        var subdir =subdirs[i].substring(directory.length +1);
+        const pattern= new RegExp('([a-z]+) - '+subdir);
+
+        
+        const files = fs.readdirSync(subdirs[i])
+        for (const file of files) {
+            console.log(file);
+            console.log(pattern.test(file));
+            try {
+            if(pattern.test(file)==false){throw new Error('File name does not match criteria.');}
+        } catch (e) {console.error(e);}
+        }
+    }
+});
+
+
+
