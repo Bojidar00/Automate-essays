4714c64f40cec3f30425a282498b678db8b09db0
Bojidar00 <bojidar_rusalinov@abv.bg>
2021-11-24 15:04:51 +0200

    Add html validation.

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 var dir = require('node-dir');
 const fs = require('fs');
+const validator = require('html-validator');
 
 var directory="essays";
 
@@ -10,18 +11,36 @@ dir.subdirs(directory, function(err, subdirs) {
         console.log(subdirs[i]);
         var subdir =subdirs[i].substring(directory.length +1);
         const pattern= new RegExp('([a-z]+) - '+subdir);
+        const html_pattern= new RegExp('([a-z]+).html');
+        const md_pattern= new RegExp('([a-z]+).md');
 
         
         const files = fs.readdirSync(subdirs[i])
         for (const file of files) {
             console.log(file);
             console.log(pattern.test(file));
-          //  try {
             if(pattern.test(file)==false){throw new Error('File name does not match criteria.');}
-       // } catch (e) {console.error(e);}
+            if(html_pattern.test(file)==true){Check_html(subdir+"/"+file);}
+            if(md_pattern.test(file)==true){}
         }
     }
 });
 
 
 
+async function Check_html  (html_file_directory) {
+    
+    const options = {
+      validator: 'WHATWG',
+      format: 'text',
+      data: fs.readFileSync(directory+"/"+html_file_directory, 'utf8')
+    }
+    
+    try {
+      const result = await validator(options)
+      console.log(result)
+    } catch (error) {
+      console.error(error)
+    }
+  }
+  
